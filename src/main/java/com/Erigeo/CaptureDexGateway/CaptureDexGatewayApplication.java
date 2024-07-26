97fb4be3df6faef0162bab929a70e3a0c29eb9df
Georg Herison <georgherison@gmail.com>
2024-07-26 12:27:11 -0300

    starting api gateway with spring cloud

---
+++ src/main/java/com/Erigeo/CaptureDexGateway/CaptureDexGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.Erigeo.CaptureDexGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CaptureDexGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaptureDexGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Erigeo/CaptureDexGateway/CaptureDexGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Erigeo.CaptureDexGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaptureDexGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
